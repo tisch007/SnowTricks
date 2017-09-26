fc1d36bfcf15f3110735278ce6094556776089e6
tisch007 <cyrille.tischenbach@gmail.com>
2017-09-26 19:20:59 +0200

    commit initial

---
+++ src/TricksBundle/Entity/Tricks.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace TricksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tricks
+ *
+ * @ORM\Table(name="tricks")
+ * @ORM\Entity(repositoryClass="TricksBundle\Repository\TricksRepository")
+ */
+class Tricks
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255, unique=true)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string", length=21000)
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="dateAjout", type="datetime")
+     */
+    private $dateAjout;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Tricks
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Tricks
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Tricks
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set dateAjout
+     *
+     * @param datetime $dateAjout
+     *
+     * @return Tricks
+     */
+    public function setDateAjout($dateAjout)
+    {
+        $this->dateAjout = $dateAjout;
+
+        return $this;
+    }
+
+    /**
+     * Get dateAjout
+     *
+     * @return datetime
+     */
+    public function getDateAjout()
+    {
+        return $this->dateAjout;
+    }
+}
+
---
+++ src/TricksBundle/TricksBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TricksBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TricksBundle extends Bundle
+{
+}
